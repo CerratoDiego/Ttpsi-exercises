77ccd11acac88bc24bf3a71a87bb9e98f80b617c
CerratoDiego <92369528+CerratoDiego@users.noreply.github.com>
2022-01-25 16:22:25 +0100

    JavaScript
    es02

---
+++ JavaScript/es02 Lancio moneta/index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function lanciaMoneta() {
+	let _txtLanci=document.getElementById("txtLanci")
+	let _lblCroce=document.getElementById("lblCroce")
+	let _lblTesta=document.getElementById("lblTesta")
+
+	let n=parseInt(_txtLanci.value)
+}
