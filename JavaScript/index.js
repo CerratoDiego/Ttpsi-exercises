3e1a71382803bad88cf8483c46c74d520e7b2e40
CerratoDiego <92369528+CerratoDiego@users.noreply.github.com>
2022-02-08 16:41:51 +0100

    JavaScript
    index

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,9 @@
 "use strict"
+
+
+
+function generaNumero(a, b) {
+	//genera un numero casuale tra a e b, estremo superiore escluso
+	let ris =  Math.floor((b - a)*Math.random()) + a
+	return ris
+}
